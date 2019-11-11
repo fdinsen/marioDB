9d3ceb3ca4278817b916b718deea9a97052d3cbc
Oliver Vang <olv.vang@gmail.com>
2019-11-11 22:02:14 +0100

    Removed IntelliJ files

--- src/main/java/datasource/DBConnector.java
+++ src/main/java/datasource/DBConnector.java
@@ -1,4 +1,34 @@
 package datasource;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DBConnector {
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/mario";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    private static Connection con = null;
+
+    private DBConnector() {
+    }
+
+    public static Connection getConnection() {
+        if (con == null) {
+            try {
+                Class.forName(DRIVER);
+                con = DriverManager.getConnection(URL, USER, PASSWORD);
+            } catch (ClassNotFoundException | SQLException ex) {
+                Logger.getLogger(DBConnector.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return con;
+    }
+
+    public static void main(String[] args) {
+        new DBConnector().getConnection();
+    }
 }
