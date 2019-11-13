95d855f5b45cdb84853853a74d48cc8f4e97a46a
Frederik Dinsen <gammarik@gmail.com>
2019-11-13 15:37:42 +0100

    Finished removeOrder() in ActiveOrderMapper

--- src/main/java/datasource/ActiveOrderMapper.java
+++ src/main/java/datasource/ActiveOrderMapper.java
@@ -74,10 +74,11 @@ public class ActiveOrderMapper {
     }
 
     public void insertOrder(Order ord) {
+        con = DBConnector.getConnection();
+        String SQL = "INSERT INTO active_orders (total_price, pickup_time, customer_phone) "
+                + "VALUES (?, ?, ?)";
         try {
-            con = DBConnector.getConnection();
-            String SQL = "INSERT INTO active_orders (total_price, pickup_time, customer_phone) "
-                    + "VALUES (?, ?, ?)";
+
             PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
             ps.setDouble(1, ord.getTotalPrice());
             ps.setString(2, ord.getPickupTime().toString());
@@ -85,35 +86,41 @@ public class ActiveOrderMapper {
             ps.execute();
             ResultSet orderId = ps.getGeneratedKeys();
             ps.close();
-            for (Pizza pizza : ord.getAllPizzasOnOrder() ) {
+            for (Pizza pizza : ord.getAllPizzasOnOrder()) {
                 SQL = "INSERT INTO orderlines_pizzas (order_id, pizza_id) VALUES (?, ?)";
                 ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
-                ps.setInt(1, orderId.getInt("order_id"));
+                int currentOrderId = orderId.getInt("order_id");
+                ps.setInt(1, currentOrderId);
                 ps.setInt(2, pizza.getPizzaNo());
                 ps.execute();
                 ResultSet orderlineId = ps.getGeneratedKeys();
                 ps.close();
-                for(Topping topping : pizza.getToppingsAdded()) {
-                    SQL = "INSERT INTO orderlines_toppings (orderline_id, topping_id) VALUES (?, ?)";
+                for (Topping topping : pizza.getToppingsAdded()) {
+                    SQL = "INSERT INTO orderlines_toppings (orderline_id, topping_id, order_id) VALUES (?, ?, ?)";
                     ps = con.prepareStatement(SQL);
                     ps.setInt(1, orderlineId.getInt("orderline_id"));
                     ps.setInt(2, topping.getToppingId());
+                    ps.setInt(3, currentOrderId);
                     ps.execute();
                     ps.close();
                 }
             }
-            
-            
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(ActiveOrderMapper.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
 
-    
-
     public void removeOrder(int orderId) {
+        con = DBConnector.getConnection();
+        String SQL = "DELETE FROM active_orders, orderlines_pizzas, orderlines_toppings WHERE order_id = ?";
+        try (PreparedStatement ps = con.prepareStatement(SQL)) {
+            ps.setInt(1, orderId);
+            ps.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(ActiveOrderMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 }
